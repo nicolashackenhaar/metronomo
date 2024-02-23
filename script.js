71e545a31d31fe46db5eb697f606e72bc15bce0c
Nicolas Hack <nicolashackenhaar@gmail.com>
2024-02-23 15:22:13 -0300

    validacoes e funcionamento do slider e botoes

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-const tempoDisplay = document.getElementsByClassName('tempo');
-const tempoText = document.getElementsByClassName('tempo-text');
-const decreaseTempoBtn = document.getElementsByClassName('decrease-tempo');
-const increaseTempoBtn = document.getElementsByClassName('increase-tempo')
-const tempoSlider = document.getElementsByClassName('slider');
-const startStopBtn = document.getElementsByClassName('slider');
-const subtractBeats = document.getElementsByClassName('subtract-beats');
-const addBeats = document.getElementsByClassName('add-beats');
-const measureCount = document.getElementsByClassName('measure-count');
+const tempoDisplay = document.querySelector('.tempo');
+const tempoText = document.querySelector('.tempo-text');
+const decreaseTempoBtn = document.querySelector('.decrease-tempo');
+const increaseTempoBtn = document.querySelector('.increase-tempo');
+const tempoSlider = document.querySelector('.slider');
+const startStopBtn = document.querySelector('.start-stop');
+const subtractBeats = document.querySelector('.subtract-beats');
+const addBeats = document.querySelector('.add-beats');
+const measureCount = document.querySelector('.measure-count');
 
 
-//valores iniciais
 let bpm = 140;
 let beatsPerMeasure = 4;
+
+decreaseTempoBtn.addEventListener('click', decreaseTempo);
+increaseTempoBtn.addEventListener('click', increaseTempo);
+tempoSlider.addEventListener('input', tempoSlide);
+
+
+
+function decreaseTempo() {
+  if(bpm <= 20 ){ return }
+  bpm--;
+  updateMetronome();
+  
+}
+
+function increaseTempo() {
+  if(bpm >= 280 ){ return }
+  bpm++;
+  updateMetronome();
+  
+}
+
+function tempoSlide() {
+  bpm = tempoSlider.value;
+  updateMetronome();
+}
+
+
+function updateMetronome() {
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+}
+
