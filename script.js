6504c43ee0a5e22e9e2d90a66e49d467efe5a624
Nicolas Hack <nicolashackenhaar@gmail.com>
2024-02-24 16:20:16 -0300

    funciona apertando o botao mas nao troca enquanto ligado

--- script.js
+++ script.js
@@ -13,25 +13,30 @@ const click2 = new Audio('click2.mp3');
 
 
 
-let bpm = 140;
-let beatsPerMeasure = 4;
+let bpm = 120;
+let contagem = 3; // tics
+let contador = 0 // onde está a batida
+let taRodando = false
 
-decreaseTempoBtn.addEventListener('click', decreaseTempo);
+
+decreaseTempoBtn.addEventListener('click', decreaseTempo,);
 increaseTempoBtn.addEventListener('click', increaseTempo);
 tempoSlider.addEventListener('input', tempoSlide);
-
+startStopBtn.addEventListener('click', start )
 
 
 function decreaseTempo() {
   if(bpm <= 20 ){ return };
   bpm--;
   updateMetronome();
+
 }
 
 function increaseTempo() {
   if(bpm >= 280 ){ return };
   bpm++;
   updateMetronome();
+
 }
 
 function tempoSlide() {
@@ -43,5 +48,33 @@ function tempoSlide() {
 function updateMetronome() {
   tempoDisplay.textContent = bpm;
   tempoSlider.value = bpm;
+  
+
 }
 
+function start(){
+  contador = 0
+  if(!taRodando){
+    intervalID = setInterval(play, 60000 / bpm)
+    taRodando = true;
+    startStopBtn.textContent = 'STOP';
+  } else {
+    startStopBtn.textContent = 'START';
+    taRodando = false;
+    clearInterval(intervalID);
+  }
+  
+}
+
+function play() {
+  
+  if(contador < contagem){
+    click1.play();
+    contador++
+    console.log(contador)
+  } else{
+    click2.play();
+    console.log(contador)
+    contador = 0
+  }
+}
