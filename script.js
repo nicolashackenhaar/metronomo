2076f39db1c993e164a8814c9897ee5f46625dd9
Nicolas Hack <nicolashackenhaar@gmail.com>
2024-02-25 09:07:37 -0300

    removendo texto de velocidade

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const tempoDisplay = document.querySelector('.tempo');
-const tempoText = document.querySelector('.tempo-text');
 const decreaseTempoBtn = document.querySelector('.decrease-tempo');
 const increaseTempoBtn = document.querySelector('.increase-tempo');
 const tempoSlider = document.querySelector('.slider');
