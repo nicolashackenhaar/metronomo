2eb52ea34074d997f0fd40e4e54689d53d32ebf1
Nicolas Hack <nicolashackenhaar@gmail.com>
2024-02-29 20:15:13 -0300

    TA FUNCIONANDO MAS FALTA ARRUMAR BATIDAS

--- script.js
+++ script.js
@@ -10,78 +10,64 @@ const measureCount = document.querySelector('.measure-count');
 const click1 = new Audio('click1.mp3');
 const click2 = new Audio('click2.mp3');
 
-
-
-let bpm = 140;
 let contagem = 3; // tics
-let contador = 0 // onde está a batida
-let taRodando = false
+let contador = 0; // onde está a batida
+let taRodando = false;
+let intervalID;
+let bpm = 140;
 
-
-decreaseTempoBtn.addEventListener('click', decreaseTempo,);
+decreaseTempoBtn.addEventListener('click', decreaseTempo);
 increaseTempoBtn.addEventListener('click', increaseTempo);
 tempoSlider.addEventListener('input', tempoSlide);
-startStopBtn.addEventListener('click', start )
-
+startStopBtn.addEventListener('click', start);
 
 function decreaseTempo() {
-  if(bpm <= 20 ){ return };
+  if(bpm <= 20) return;
   bpm--;
   updateMetronome();
-
 }
 
 function increaseTempo() {
-  if(bpm >= 280 ){ return };
+  if(bpm >= 280) return;
   bpm++;
   updateMetronome();
-
 }
 
 function tempoSlide() {
   bpm = tempoSlider.value;
   updateMetronome();
-  console.log(bpm)
 }
-
 
 function updateMetronome() {
   tempoDisplay.textContent = bpm;
   tempoSlider.value = bpm;
-  bpm = tempoSlider.value;
+  clearInterval(intervalID);
+  if (taRodando) {
+    intervalID = setInterval(play, 60000 / bpm);
+  }
 }
 
-
-
-
-function start(){
-  contador = 3
-
-  if(!taRodando){
-    intervalID = setInterval(play, 60000 / bpm)
+function start() {
+  if (!taRodando) {
+    intervalID = setInterval(play, 60000 / bpm);
     taRodando = true;
     startStopBtn.textContent = 'STOP';
   } else {
     startStopBtn.textContent = 'START';
     taRodando = false;
     clearInterval(intervalID);
+    contador = 0; 
   }
-  
 }
 
 function play() {
-  
-  if(contador < contagem){
+  if (contador < contagem) {
     click1.play();
-    contador++
-    console.log(contador)
-  } else{
+    contador++;
+    console.log(contador);
+  } else {
     click2.play();
-    console.log(contador)
-    contador = 0
-    
-    
-
+    contador = 0;
+    console.log(contador);
   }
-
 }
